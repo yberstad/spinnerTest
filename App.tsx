1e36c55040f0a88ba16aa143a13de7ff37587616
Øyvind Habberstad <oyvind@sharpnet.no>
2021-04-21 09:05:18 +0200

    CircleSnail memory test

---
+++ App.tsx
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+import {CircleSnail} from 'react-native-progress';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <CircleSnail style={styles.progress} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  circles: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  progress: {
+    margin: 10,
+  },
+});
+
+export default App;
